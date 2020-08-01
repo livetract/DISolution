e47727557f006c0997054681985d84bb8834ecbc
YD <abc_hyd@outlook.com>
2020-08-01 10:59:41 +0800

    恢复require

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,19 +15,19 @@ namespace ConsoleApp
                     .AddScoped<ITestService, SignInManager>()
                     .BuildServiceProvider();    // 创建服务容器
 
-                //var userSer = sp.GetRequiredService<ITestService>();
+                var userSer = sp.GetRequiredService<ITestService>();
 
-                var userNoRequire = sp.GetService<ITestService>();  // 使用这个方法，需要自己判断是否获取了空的服务。导致返回值为空。
-                if (userNoRequire != null)
-                {
-                    var u3 = userNoRequire.Signin("jim", "123");
-                }
-                else
-                {
-                    throw new NullReferenceException(nameof(userNoRequire));
-                }
-                //var u1 = userSer.Signin("jim","134");
-                //var u2 = userSer.Signin("jim", "123");
+                //var userNoRequire = sp.GetService<ITestService>();  // 使用这个方法，需要自己判断是否获取了空的服务。导致返回值为空。
+                //if (userNoRequire != null)
+                //{
+                //    var u3 = userNoRequire.Signin("jim", "123");
+                //}
+                //else
+                //{
+                //    throw new NullReferenceException(nameof(userNoRequire));
+                //}
+                var u1 = userSer.Signin("jim", "134");
+                var u2 = userSer.Signin("jim", "123");
 
 
             }
