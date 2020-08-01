c9eefd965e7b118af14522cef07f7fdcc84f94e5
YD <abc_hyd@outlook.com>
2020-08-01 10:54:05 +0800

    GetRequireService可以引发错误异常，GetService需要手动判断。

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -16,8 +16,13 @@ namespace ConsoleApp
                     .BuildServiceProvider();    // 创建服务容器
 
                 var userSer = sp.GetRequiredService<ITestService>();
+
+                var userNoRequire = sp.GetService<ITestService>();  // 使用这个方法，需要自己判断是否获取了空的服务。导致返回值为空。
+
                 var u1 = userSer.Signin("jim","134");
                 var u2 = userSer.Signin("jim", "123");
+
+                var u3 = userNoRequire.Signin("jim", "123");
             }
             catch (Exception ex)
             {
