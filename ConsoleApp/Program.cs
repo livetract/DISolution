77e7a4b90ddcc873fea3feb482f4d049f7b6271b
YD <abc_hyd@outlook.com>
2020-08-01 10:23:30 +0800

    引入di

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
