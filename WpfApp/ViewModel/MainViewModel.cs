02cfe5e018b28a6b442d77853e0462d29565ba64
Hu <Hu@DESKTOP-0V9DP6B>
2020-08-07 00:39:47 +0800

    手动实现RelayCommand

--- WpfApp/ViewModel/MainViewModel.cs
+++ WpfApp/ViewModel/MainViewModel.cs
@@ -13,12 +13,24 @@ namespace WpfApp.ViewModel
             new Module{Name = "照片" },
             new Module{Name = "收藏" }
             };
+
+            OpenCommand = new RelayCommand<string>(t => OpenPage(t));
         }
 
+
+        public object Page { get; set; }
 
         /// <summary>
         /// 配置功能
         /// </summary>
         public List<Module> Modules { get; set; }
+
+
+        public RelayCommand<string> OpenCommand { get; set; }
+
+        public void OpenPage(string name)
+        {
+
+        }
     }
 }
---
+++ WpfApp/ViewModel/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModel
+{
+    public class RelayCommand<T>
+        : ICommand
+        where T : class
+    {
+        private readonly Action<T> excute;
+        private readonly Func<T, bool> canExcute;
+
+        public RelayCommand(Action<T> excute, Func<T, bool> canExcute = null)
+        {
+            this.excute = excute;
+            this.canExcute = canExcute;
+        }
+
+        public event EventHandler CanExecuteChanged = (s, e) => { };
+
+        public bool CanExecute(object parameter)
+        => canExcute == null || canExcute((T)parameter);
+
+        public void Execute(object parameter)
+        => excute((T)parameter);
+    }
+}
