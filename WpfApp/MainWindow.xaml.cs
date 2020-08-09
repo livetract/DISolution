0f41438914d05280bb832b79509f871516fdcfbd
livetract <67226673+livetract@users.noreply.github.com>
2020-08-10 00:51:51 +0800

    处理大小控制

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using WpfApp.ViewModel;
 
@@ -17,7 +19,7 @@ namespace WpfApp
             this.DataContext = new MainViewModel();
         }
 
-        private void Border_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        private void Border_MouseMove(object sender, MouseEventArgs e)
         {
             // 点击鼠标左键拖动界面
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -26,11 +28,34 @@ namespace WpfApp
             }
         }
 
-        private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
             // 根据拖动的增量改变
             Width += e.HorizontalChange;
             Height += e.VerticalChange;
+        }
+
+        private void ClickBtnConsole(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var tag = btn.Tag;
+            switch (tag)
+            {
+                case "ToBar":
+                    this.WindowState = WindowState.Minimized;
+                    break;
+                case "Max":
+                    this.WindowState = WindowState.Maximized;
+                    ((Button)sender).Tag = "Reduction";
+                    break;
+                case "Reduction":
+                    this.WindowState = WindowState.Normal;
+                    ((Button)sender).Tag = "Max";
+                    break;
+                case "Close":
+                    this.Close();
+                    break;
+            }
         }
     }
 }
