118be3f3222acd702b1818053b4430b92254ddac
livetract <67226673+livetract@users.noreply.github.com>
2020-08-11 17:38:40 +0800

    伸缩侧边栏失败

---
+++ WpfApp/Converters/MenuWidthCoverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp.Converters
+{
+    public class MenuWidthCoverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && bool.TryParse(value.ToString(), out bool result))
+            {
+                if (result)
+                    return 250;
+            }
+            return 60;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
